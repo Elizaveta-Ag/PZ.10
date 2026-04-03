1d323d1674032ae3da4674194bc52d88e478e9cc
Elizaveta-Ag <elizavetaageeva882@gmail.com>
2026-04-03 13:17:59 +0300

    Create пз10_3.cpp

---
+++ пз10_3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    int N;
+    cin >> N;
+    
+    for (int i = N; i >= 1; i--) {
+       cout << i << " "; 
+}
+
+    return 0;
+}
